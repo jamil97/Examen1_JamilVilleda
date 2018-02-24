bcc1f064bed670ca0fa0ceff1307b918a1fb998d
jamil97 <jamvilleda97@live.com>
2018-02-23 18:20:34 -0600

    Con el programa a medio paso

--- Examen1Lab_JamilVilleda/src/examen1lab_jamilvilleda/Examen1Lab_JamilVilleda.java
+++ Examen1Lab_JamilVilleda/src/examen1lab_jamilvilleda/Examen1Lab_JamilVilleda.java
@@ -20,7 +20,7 @@ public class Examen1Lab_JamilVilleda {
      * @param args the command line arguments
      */
     static Scanner sc = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
         String comando = "";
         String nombrecarpeta = "";
@@ -34,20 +34,20 @@ public class Examen1Lab_JamilVilleda {
         System.out.println("Ingrese el size: ");
         int size = sc.nextInt();
         lista.add(new SistemaArchivos(nombre, usuario, size));
-        
+
         while (true) {
-            
+
             System.out.print(usuario + "/" + "root" + "/");
             sc.nextLine();
             comando = sc.nextLine();
             String[] comando2 = comando.split(" ");
-            
+
             for (int i = 0; i < comando2.length; i++) {
                 if (comando2[i].equalsIgnoreCase("exit")) {
                     System.exit(0);
                 }
             }
-            
+
             if ("mkdir".equalsIgnoreCase(comando2[0])) {
                 String[] palabra = comando.split(" ");
                 for (String direccion : palabra) {
@@ -58,24 +58,34 @@ public class Examen1Lab_JamilVilleda {
                 System.out.println("Se ha creado exitosamente una carpeta de nombre: " + nombrecarpeta + "\n"
                         + "Fecha creacion: " + fecha);
             }
-            
+
             if ("del".equalsIgnoreCase(comando2[0])) {
                 String[] palabra = comando.split("  ");
-                
+
             }
-            
+
             if ("cat".equalsIgnoreCase(comando2[0])) {
                 String palabra = comando.substring(comando.length() - 4, comando.length());
                 String palabra2 = comando.substring(comando.length() - 5, comando.length());
                 if (palabra.equalsIgnoreCase(".txt")) {
-                    
-                } else if (palabra.equalsIgnoreCase(".exec")){
-                    
+
+                    System.out.println("Ingrese lo que desea agregar al archivo de texto: ");
+                    String mensaje = sc.nextLine();
+                    lista.add(new ArchivoTexto(mensaje));
+                    System.out.println("Se ha agregado el archivo de texto correctamente.");
+
+                } else if (palabra2.equalsIgnoreCase(".exec")) {
+
+                    System.out.println("Ingrese todos lo comandos a realizar: ");
+                    String cm = sc.nextLine();
+                    String[] comandos = cm.split("0");
+                    for (String comando1 : comandos) {
+                        System.out.println(comando1);
+
+                    }
                 }
-             
-                
             }
-            
+
             if ("cd".equalsIgnoreCase(comando2[0])) {
                 String[] palabra = comando.split(" ");
                 for (String string : palabra) {
@@ -84,7 +94,23 @@ public class Examen1Lab_JamilVilleda {
 //                    }                    
                 }
             }
-            
+
+            if ("ls".equalsIgnoreCase(comando2[0])) {
+                for (Object t : lista) {
+                    System.out.println(t);
+                }
+            }
+
+            if ("mod".equalsIgnoreCase(comando2[0])) {
+                String[] palabra = comando.split(" ");
+                for (String string : palabra) {
+
+                    String txt = palabra[1];
+      
+                    
+                }
+
+            }
         }
     }
 }
